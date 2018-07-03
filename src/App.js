1bd75609077384ff1be187a473ee3e2675d773fd
Juliusz Jakubowski <asfalt09@gmail.com>
2018-07-03 09:26:11 +0200

    little styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import Markdown from 'markdown-to-jsx';
 
-import '../styles/app.css';
+import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import styled from 'styled-components';
+
+import 'brace/mode/markdown';
+import 'brace/theme/monokai';
+import './styles/app.css';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -19,8 +24,45 @@ class App extends Component {
   }
 
   render() {
-    return <Markdown>{this.state.loadedFile}</Markdown>;
+    return (
+      <Split>
+        <CodeWindow>
+          <AceEditor
+            mode="markdown"
+            theme="monokai"
+            onChange={newContent => {
+              this.setState({ loadedFile: newContent });
+            }}
+            name="mardown_editor"
+            value={this.state.loadedFile}
+          />
+        </CodeWindow>
+
+        <RenderedWindow>
+          <Markdown>{this.state.loadedFile}</Markdown>
+        </RenderedWindow>
+      </Split>
+    );
   }
 }
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background: #2f3129;
+`;
+
+const RenderedWindow = styled.div`
+  background: #2f3129;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #00ff00;
+`;
