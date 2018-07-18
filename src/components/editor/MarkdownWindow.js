452856d1aedb1f43ed75ecab2bf41c9d9395679e
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-07-18 21:55:21 +0200

    making MarkdownWindow a stateless component

--- src/components/editor/MarkdownWindow.js
+++ src/components/editor/MarkdownWindow.js
@@ -1,20 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Markdown from 'markdown-to-jsx';
 import styled from 'styled-components';
 
-import store from '../../store';
 
-class MarkdownWindow extends Component {
-    render() {
-        const input = store.getState().editorInputReducer.result || '';
-
-        return (
-            <RenderedWindow>
-                <Markdown>{input}</Markdown>
-            </RenderedWindow>
-        )
-    }
-}
+const MarkdownWindow = ({content}) => (
+    <RenderedWindow>
+        <Markdown>{content}</Markdown>
+    </RenderedWindow>
+);
 
 export default MarkdownWindow;
 
