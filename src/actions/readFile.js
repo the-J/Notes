9adbad1effe4bf699eba8b4a3202c0b401b8846b
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-07-21 13:33:30 +0200

    updating readFileAction to handle operation of reading file

--- src/actions/readFile.js
+++ src/actions/readFile.js
@@ -1,6 +1,13 @@
-export const readFile = (fileContent = '') => dispatch => {
-    dispatch({
-        type: 'READ_FILE',
-        payload: fileContent
-    });
+import readFileMethod from '../methods/readFileMethod';
+
+export const readFile = path => dispatch => {
+    if (path && 'string' === typeof path) {
+        readFileMethod(path, fileContent => {
+            dispatch({
+                type: ' READ_FILE',
+                payload: fileContent || ''
+            });
+        });
+    }
+    else console.error('readFileMethod no path provided');
 };
