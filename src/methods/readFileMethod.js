374529743ec00ceb03c9b7fd345e260e187fb694
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-07-23 20:40:24 +0200

    updating to new naming convention

--- src/methods/readFileMethod.js
+++ src/methods/readFileMethod.js
@@ -3,14 +3,13 @@ const fs = window.require('fs');
 /**
  *
  * @param path {String}
- * @param cb {String} - caries file data
+ * @param cb - caries file data
  */
 const readFileMethod = (path, cb) => {
     if (!path || 'string' !== typeof path) return console.log('no file path');
 
     fs.readFile(path, (err, data) => {
         if (err) console.error('readFileMethod err:', err);
-
         return cb(data.toString());
     });
 };
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 
-import loadFiles  from './loadFiles';
-import readFile  from './readFile';
+import loadFiles from './loadFiles';
+import readFile from './readFile';
 
 export default combineReducers({
     filesList: loadFiles,
